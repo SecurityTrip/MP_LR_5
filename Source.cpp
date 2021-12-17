521bc71b440531e19be308880199a7092e8b1de4
Илья Лысов <51211214+SecurityTrip@users.noreply.github.com>
2021-12-17 22:44:34 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -121,6 +121,34 @@ void parce(std::string in, std::string& n ,int& d, std::vector<double>& v)
 	}
 }
 
+double check_in(char f, int n, double d)
+{
+	if (f == 'i')
+	{
+		std::cout << "Enter days count > "; std::cin >> n;
+		while ((std::cin.fail()) || (std::cin.peek() != '\n' || n < 0))
+		{
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+			std::cout << "Wrong type of input data, try again: "; std::cin >> n;
+			std::cout << std::endl;
+		}
+		return n;
+	}
+	if (f == 'd')
+	{
+		std::cin >> d;
+		while ((std::cin.fail()) || (std::cin.peek() != '\n' || n < 0))
+		{
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+			std::cout << "Wrong type of input data, try again: "; std::cin >> d;
+			std::cout << std::endl;
+		}
+		return d;
+	}
+}
+
 int main()
 {
 	std::cout << "Information about profit" << std::endl;
@@ -143,6 +171,8 @@ int main()
 						std::cout << "Enter input file name>"; std::cin >> in_name;					
 						in_file.open(in_name);
 						if (!in_file.is_open()) { std::cout << "Can`t open input file." << std::endl; goto repeat_in_1_1; }
+						std::cin.clear();
+						std::cin.ignore(10000, '\n');
 
 					repeat_out_1_1:
 						std::cout << "Enter output file name>"; std::cin >> out_name;
@@ -159,7 +189,10 @@ int main()
 
 						profit prof(name,days,vec);
 
-						out_file << prof.non_profit();
+						out_file << "Days without profit: " << prof.non_profit();
+
+						in_file.close();
+						out_file.close();
 
 					break;
 					}
@@ -173,6 +206,18 @@ int main()
 						in_file.open(in_name);
 						if (!in_file.is_open()) { std::cout << "Can`t open input file." << std::endl; goto repeat_in_1_2; }
 					
+						std::string file_data;
+						getline(in_file, file_data);
+						std::string name;
+						int days;
+						std::vector<double> vec;
+						parce(file_data, name, days, vec);
+
+						profit prof(name, days, vec);
+
+						std::cout << "Days without profit: " << prof.non_profit();
+
+						in_file.close();
 						
 					break;
 					}
@@ -192,19 +237,48 @@ int main()
 						std::cout << "Enter output file name>"; std::cin >> out_name;
 						out_file.open(out_name);
 						if (!out_file.is_open()) std::cout << "Can`t open output file." << std::endl; goto repeat_out_2;
-						
+
+						std::cout << "Enter branch number> "; std::string branch; std::cin >> branch;
+						double a = 0.0;
+						std::cout << "Enter days count> "; int days; days = (int)check_in('i',days, a);
+						std::vector<double> vec;
+						int b = 0;
+						for (int i = 0; i < days; i++) 
+						{
+							std::cout << "Enter profit in " << i + 1 << " day>"; double p;  p = check_in('d', b, p);
+							vec.push_back(p);
+						}
+
+						profit prof(branch, days, vec);
+
+						out_file << "Days without profit: " << prof.non_profit();
+
+						out_file.close();
+
 					break;
 					}
 				case 2:
 					{
 
+						std::cout << "Enter branch number> "; std::string branch; std::cin>> branch;
+						double a = 0.0;
+						std::cout << "Enter days count> "; int days = 0; days = (int)check_in('i', days, a);
+						std::vector<double> vec;
+						int b = 0;
+						for (int i = 0; i < days; i++)
+						{
+							std::cout << "Enter profit in " << i + 1 << " day>"; double p = 0;  p = check_in('d', b, p);
+							vec.push_back(p);
+						}
+
+						profit prof(branch, days, vec);
+
+						std::cout << "Days without profit: " << prof.non_profit();
 					break;
 					}
 			}
 			break;
 		}
-
 	}
-	
-	
+
 }
