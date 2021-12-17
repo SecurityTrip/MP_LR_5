83cfe8c2592319d30724fca6c25db42c6acebff4
Илья Лысов <Lysovidq@gmail.com>
2021-12-17 21:46:21 +0400

    v 0.3

--- Source.cpp
+++ Source.cpp
@@ -2,24 +2,27 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+
 
 class profit 
 {
 private:
-	int number, days_count;
+	std::string number;
+	int days_count;
 	std::vector<double> profit_per_day;
 public:
 	profit()
 	{
-		number = 0;
+		number = "0";
 		days_count = 0;
 	};
-	profit(int num, int days, std::vector<double> day_profit)
+	profit(std::string num, int days, std::vector<double> day_profit)
 	{
 		number = num;
 		days_count = days;
 		profit_per_day = day_profit;
-	};
+	}
 
 	profit(const profit& p)// конструктор копирования
 	{
@@ -40,7 +43,7 @@ public:
 
 	
 
-	int non_profit(std::vector<double> profit_per_day)
+	int non_profit()
 	{
 		int non = 0;
 		for (int i = 0; i < profit_per_day.size(); i++)
@@ -70,7 +73,7 @@ std::ostream& operator<< (std::ostream& out, const profit& p)
 	
 	for (int i = 0; i < p.profit_per_day.size(); i++)
 	{
-		out << p.profit_per_day[i];
+		out << p.profit_per_day[i] << " ";
 	}
 	out << std::endl;
 	return out;
@@ -81,7 +84,7 @@ int cheker(int& in, std::string arg)
 	in = 0;
 	while (in != 1 && in != 2)
 	{
-		std::cout << "Chose " << arg << " type(file / consele)>"; std::string in_type_str; std::cin >> in_type_str;
+		std::cout << "Choose " << arg << " type(file / consele)>"; std::string in_type_str; std::cin >> in_type_str;
 		if (in_type_str == "file")
 		{
 			in = 1;
@@ -98,13 +101,110 @@ int cheker(int& in, std::string arg)
 	return in;
 }
 
+void parce(std::string in, std::string& n ,int& d, std::vector<double>& v)
+{
+	std::istringstream input{ in };
+	std::vector<std::string> result;
+
+	while (!input.eof()) {
+		std::string substring;
+		input >> substring;
+		result.push_back(substring);
+	}
+
+	n = result[0];
+	d = stoi(result[1]);
+
+	for (int i = 2; i < result.size(); i++)
+	{
+		v.push_back(atof(result[i].c_str()));
+	}
+}
+
 int main()
 {
 	std::cout << "Information about profit" << std::endl;
 	int in_type = cheker(in_type, "input");
 	int out_type = cheker(out_type, "output");
 
-	std::cout << in_type << std::endl << out_type;
+	switch (in_type)
+	{
+		case 1:
+		{
+			switch (out_type)
+			{
+				case 1:
+					{
+						std::ifstream in_file;
+						std::ofstream out_file;
+						std::string in_name, out_name;
+
+					repeat_in_1_1:
+						std::cout << "Enter input file name>"; std::cin >> in_name;					
+						in_file.open(in_name);
+						if (!in_file.is_open()) { std::cout << "Can`t open input file." << std::endl; goto repeat_in_1_1; }
+
+					repeat_out_1_1:
+						std::cout << "Enter output file name>"; std::cin >> out_name;
+						out_file.open(out_name);
+						if (!out_file.is_open()) { std::cout << "Can`t open output file." << std::endl; goto repeat_out_1_1; }
+
+					
+						std::string file_data;
+						getline(in_file,file_data);
+						std::string name;
+						int days;
+						std::vector<double> vec;
+						parce(file_data, name, days, vec);
+
+						profit prof(name,days,vec);
+
+						out_file << prof.non_profit();
+
+					break;
+					}
+				case 2:
+					{
+						std::ifstream in_file;
+						std::string in_name;
+
+					repeat_in_1_2:
+						std::cout << "Enter input file name>"; std::cin >> in_name;
+						in_file.open(in_name);
+						if (!in_file.is_open()) { std::cout << "Can`t open input file." << std::endl; goto repeat_in_1_2; }
+					
+						
+					break;
+					}
+
+			}
+			break;
+		}
+		case 2:
+		{
+			switch (out_type)
+			{
+				case 1:
+					{
+						std::ofstream out_file;
+						std::string out_name;
+					repeat_out_2:
+						std::cout << "Enter output file name>"; std::cin >> out_name;
+						out_file.open(out_name);
+						if (!out_file.is_open()) std::cout << "Can`t open output file." << std::endl; goto repeat_out_2;
+						
+					break;
+					}
+				case 2:
+					{
+
+					break;
+					}
+			}
+			break;
+		}
+
+	}
 	
 	
 }
